f2a46b8667485d3ecd6f8d458e295218168bafda
MrSuchAsf <mrsuchasf@gmail.com>
2026-09-19 21:46:13 +0300

    Dodanie funkcjii  dla mappingu na category/product/amount

--- main.py
+++ main.py
@@ -7,16 +7,40 @@ logging.basicConfig(level=logging.DEBUG)
 from FunPayCardinal.FunPayAPI import Account, Runner
 from FunPayCardinal.FunPayAPI.updater.events import NewMessageEvent
 from FunPayCardinal.FunPayAPI.common.enums import MessageTypes
+from FunPayCardinal.FunPayAPI.types import Order
 from config import GOLDEN_KEY
 
 acc = Account(GOLDEN_KEY).get() #type: ignore
-print(f"✅ Вошли в FunPay: {acc.username}")
+
+CATEGORY_MAP = {
+    "Сеты": "sets",
+    "Сопровождение": "sopr",
+    "Буст баланса": "bust",
+}
+
+def fp_order(order: Order) -> tuple[str, str, int] | None:
+    type_field = order.fields.get("type")
+    summary_field = order.fields.get("summary")
+
+    if not type_field or not summary_field:
+        return None
+
+    if not isinstance(type_field.value, str):
+        return None
+    category = CATEGORY_MAP.get(type_field.value)
+    if category is None:
+        return None
+
+    if not isinstance(summary_field.value, dict):
+        return None
+    product = summary_field.value.get("ru", "None Lol")
+
+    amount = order.amount
+    return category, product, amount
 
 runner = Runner(acc)
 # Запускаем фоновый поток, который реально обрабатывает очередь запросов
 threading.Thread(target=runner.loop, daemon=True).start()
-
-print("🚀 Runner запущен. Жду события...")
 
 for event in runner.listen(requests_delay=4):
     if isinstance(event, NewMessageEvent):
@@ -29,5 +53,17 @@ for event in runner.listen(requests_delay=4):
                 print("Number of order: ", order_id)
 
                 order = acc.get_order(order_id)
+                
                 print("Value: ", vars(order))
-            
+                print("Subcategory: ", vars(order.subcategory))
+                print("--------------------------")
+                for key, field in order.fields.items():
+                    print(f"Field: {key}: ", vars(field))
+
+                wynik = fp_order(order)
+                print("Order: ", wynik)
+                
+
+
+
+    
